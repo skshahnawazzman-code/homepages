7e57002a10beba98460d5d7d814fa6283a14eb82
skshahnawazzman-code <skshahnawazzman@gmail.com>
2025-11-24 14:24:34 +0530

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -1,9 +1,4 @@
 
 document.getElementById("submitBtn6").addEventListener("click", function () {
-
-      window.location.href = "file:///D:/xampp/htdocs/myproject/Receipent%20Master/Recepient%20Master.html";
-    })
-document.getElementById("submitBtn5").addEventListener("click", function () {
-
-      window.location.href = "file:///D:/xampp/htdocs/myproject/Supplier%20details/Supplier%20Profile%20Details.html";
-    })
+    window.location.href = "RecepientMaster.html";
+});
