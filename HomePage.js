0fff8c4e08eccf997d430d743dec3810c13a2b0b
skshahnawazzman-code <skshahnawazzman@gmail.com>
2025-11-25 01:49:18 +0530

    Add files via upload

---
+++ HomePage.js
@@ -0,0 +1,10 @@
+
+document.getElementById("submitBtn6").addEventListener("click", function () {
+
+      window.location.href = "file:///D:/xampp/htdocs/myproject/Home%20page/RecepientMaster.html";
+    })
+document.getElementById("submitBtn5").addEventListener("click", function () {
+
+      window.location.href = "file:///D:/xampp/htdocs/myproject/Supplier%20details/Supplier%20Profile%20Details.html";
+    })
+
